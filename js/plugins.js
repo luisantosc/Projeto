47dd861860265ee66f60088758f35b26b54c75d2
luisantosc <luisantosc2@gmail.com>
2017-11-21 21:39:10 -0200

    M8

--- js/plugins.js
+++ js/plugins.js
@@ -1,5 +1,11 @@
 ;(function($){
 	$.fn.add = function(id,obj){
-
+		minhas =[];
+		$.each(series,function(i,serie){
+			if(series.id == id){
+				minhas.push(serie);
+				return minhas;
+			}
+		});
 	}
 })(jQuery);
--- js/series.js
+++ js/series.js
@@ -1,35 +1,46 @@
-var series =[{“id”:1,”nome”:"Arrow","poster":"./posters/arrow.jpg",”categoria”:”Ação e aventura”},
-		{“id”:1,”nome”:"Beauty & the beast","poster":"./posters/b&b.jpg",”categoria”:”Ação e aventura”},
-		{“id”:1,”nome”:"Bitten","poster":"./posters/bitten.jpg",”categoria”:”Drama/Ficção”},
-		{“id”:1,”nome”:"Bates Motel","poster":"./posters/bm.jpg",”categoria”:”Drama/Suspense”},
-		{“id”:1,”nome”:"Castle","poster":"./posters/castle.jpg", “categoria”:”Policial/”Ação”},
-		{“id”:1,”nome”:"Daredevil","poster":"./posters/dd.jpg",”categoria”:”Ação/Ficção”},
-		{“id”:1,”nome”:"Dexter","poster":"./posters/dexter.jpg",”categoria”:”Drama”},
-		{"id:1,”nome”:"Flash","poster":"./posters/flash.jpg",”categoria”:"Ação/Aventura”},
-		{"id”:1,”nome”:"Grey's Anatomy","poster":"./posters/ga.jpg","categoria":”Drama”},
-		{“id”:1,”nome”:"Game of Thrones","poster":"./posters/got.jpg","categoria":”Ação/Drama”},
-		{“id”:1,”nome”:"Gotham","poster":"./posters/gotham.jpg",”categoria”:”Ação/Aventura”},
-		{“id”:1,”nome”:"Hannibal","poster":"./posters/hannibal.jpg",”categoria”:”Suspense”},
-		{“id”:1,”nome”:"How I met your mother","poster":"./posters/himym.png",”categoria”:”Comédia”},
-		{“id”:1,”nome”:"House","poster":"./posters/house.jpg",”categoria”:”Drama/Comédia”},
-		{“id”:1,”nome”:"Jessica Jones","poster":"./posters/jj.jpg",”categoria”:”Ação/Drama”},
-		{“id”:1,”nome”:"Narcos","poster":"./posters/narcos.jpg",”categoria”:”Drama”},
-		{“id”:1,”nome”:"Orange is the new black","poster":"./posters/oitn.jpg",”categoria”:”Comédia/Drama”},
-		{“id”:1,”nome”:"Once upon a time","poster":"./posters/ouat.jpg",”categoria”:”Drama/Ação/Fantasia”},
-		{“id”:1,”nome”:"Penny Dreadful","poster":"./posters/pd.jpg",”categoria”:”Suspense/Terror”},
-		{“id”:1,”nome”:"Revenge","poster":"./posters/revenge.jpg",”categoria”:”Drama”},
-		{“id”:1,”nome”:"Sense8","poster":"./posters/s8.jpg",”categoria”:Ficção/Ação”},
-		{“id”:1,”nome”:"Salem","poster":"./posters/salam.jpg",”categoria”:”Drama/Suspense/Terror”},
-		{“id”:1,”nome”:"Scandal","poster":"./posters/scandal.jpg",”categoria”:”Drama”},
-		{“id”:1,”nome”:"Scream","poster":"./posters/scream.jpg",”categoria”:”Suspense/Terror”},
-		{“id”:1,”nome”:"Shadowhunters","poster":"./posters/sh.jpg",”categoria”:”Ficção/Ação”},
-		{“id”:1,”nome”:"Sherlock","poster":"./posters/sherlock.jpg",”categoria”:”Drama/Policial”},
-		{“id”:1,”nome”:"Smallville","poster":"./posters/Smallville.jpg",”categoria”:”Drama/Ação”},
-		{“id”:1,”nome”:"Stranger things","poster":"./posters/st.jpg",”categoria”:”Suspense/Terror/Ação”},
-		{“id”:1,”nome”:"Supernatural","poster":"./posters/supernatural.jpg",”categoria”:”Ficção/Suspense”},
-		{“id”:1,”nome”:"Thirteen Reasons why","poster":"./posters/trw.jpg",”categoria”:”Drama”},
-		{“id”:1,”nome”:"Teen Wolf","poster":"./posters/tw.jpg",”categoria”:”Ação/Ficção”},
-		{“id”:1,”nome”:"The Walking Dead","poster":"./posters/twd.jpg",”categoria”:Ficção/Terror”},
-		{“id”:1,”nome”:"Vikings","poster":"./posters/vikings.jpg",”categoria”:”Ação/Aventura”}
+var series =[{"id":1,"nome":"Arrow","poster":"./posters/arrow.jpg","categoria":"Ação/Aventura"},
+		{"id":2,"nome":"Beauty & the beast","poster":"./posters/b&b.jpg","categoria":"Ação/Aventura"},
+		{"id":3,"nome":"Bitten","poster":"./posters/bitten.jpg","categoria":"Drama/Ficção"},
+		{"id":4,"nome":"Bates Motel","poster":"./posters/bm.jpg","categoria":"Drama/Suspense"},
+		{"id":5,"nome":"Castle","poster":"./posters/castle.jpg", "categoria":"Policial/Ação"},
+		{"id":6,"nome":"Daredevil","poster":"./posters/dd.jpg","categoria":"Ação/Ficção"},
+		{"id":7,"nome":"Dexter","poster":"./posters/dexter.jpg","categoria":"Drama"},
+		{"id":8,"nome":"Flash","poster":"./posters/flash.jpg","categoria":"Ação/Aventura"},
+		{"id":9,"nome":"Grey's Anatomy","poster":"./posters/ga.jpg","categoria":"Drama"},
+		{"id":10,"nome":"Game of Thrones","poster":"./posters/got.jpg","categoria":"Ação/Drama"},
+		{"id":11,"nome":"Gotham","poster":"./posters/gotham.jpg","categoria":"Ação/Aventura"},
+		{"id":12,"nome":"Hannibal","poster":"./posters/hannibal.jpg","categoria":"Suspense"},
+		{"id":13,"nome":"How I met your mother","poster":"./posters/himym.png","categoria":"Comédia"},
+		{"id":14,"nome":"House","poster":"./posters/house.jpg","categoria":"Drama/Comédia"},
+		{"id":15,"nome":"Jessica Jones","poster":"./posters/jj.jpg","categoria":"Ação/Drama"},
+		{"id":16,"nome":"Narcos","poster":"./posters/narcos.jpg","categoria":"Drama"},
+		{"id":17,"nome":"Orange is the new black","poster":"./posters/oitn.jpg","categoria":"Comédia/Drama"},
+		{"id":18,"nome":"Once upon a time","poster":"./posters/ouat.jpg","categoria":"Drama/Ação/Fantasia"},
+		{"id":19,"nome":"Penny Dreadful","poster":"./posters/pd.jpg","categoria":"Suspense/Terror"},
+		{"id":20,"nome":"Revenge","poster":"./posters/revenge.jpg","categoria":"Drama"},
+		{"id":21,"nome":"Sense8","poster":"./posters/s8.jpg","categoria":"Ficção/Ação"},
+		{"id":22,"nome":"Salem","poster":"./posters/salam.jpg","categoria":"Drama/Suspense/Terror"},
+		{"id":23,"nome":"Scandal","poster":"./posters/scandal.jpg","categoria":"Drama"},
+		{"id":24,"nome":"Scream","poster":"./posters/scream.jpg","categoria":"Suspense/Terror"},
+		{"id":25,"nome":"Shadowhunters","poster":"./posters/sh.jpg","categoria":"Ficção/Ação"},
+		{"id":26,"nome":"Sherlock","poster":"./posters/sherlock.jpg","categoria":"Drama/Policial"},
+		{"id":27,"nome":"Smallville","poster":"./posters/Smallville.jpg","categoria":"Drama/Ação"},
+		{"id":28,"nome":"Stranger things","poster":"./posters/st.jpg","categoria":"Suspense/Terror/Ação"},
+		{"id":29,"nome":"Supernatural","poster":"./posters/supernatural.jpg","categoria":"Ficção/Suspense"},
+		{"id":30,"nome":"Thirteen Reasons why","poster":"./posters/trw.jpg","categoria":"Drama"},
+		{"id":31,"nome":"Teen Wolf","poster":"./posters/tw.jpg","categoria":"Ação/Ficção"},
+		{"id":32,"nome":"The Walking Dead","poster":"./posters/twd.jpg","categoria":"Ficção/Terror"},
+		{"id":33,"nome":"Vikings","poster":"./posters/vikings.jpg","categoria":"Ação/Aventura"}
 
+];
+
+categorias = ["Ação"
+			,"Aventura"
+			,"Drama"
+			,"Terror"
+			,"Ficção"
+			,"Policial"
+			,"Fantasia"
+			,"Suspense"
+			,"Comédia"
 ];
